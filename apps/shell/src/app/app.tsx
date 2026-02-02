e2a5ab944199200d51a6de0067cfbd9f049654f2
Arunesh Saxena <aruneshsaxena@gmail.com>
2026-02-02 10:17:25 +0530

    counter redux for shell

--- apps/shell/src/app/app.tsx
+++ apps/shell/src/app/app.tsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 import NxWelcome from './nx-welcome';
 import { Link, Route, Routes } from 'react-router-dom';
 import { Button } from '@nx-repo/shared-ui';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement } from './counter/counter.slice';
 const Orders = React.lazy(() => import('orders/Module'));
 
 const Products = React.lazy(() => import('products/Module'));
 
 export function App() {
+const value = useSelector((state: any) => state.counter.value);
+const dispatch = useDispatch();
   return (
     <React.Suspense fallback={null}>
       <ul>
@@ -22,6 +25,7 @@ export function App() {
         </li>
       </ul>
       <Button label="Click Me shell" onClick={() => console.log('Button clicked! Shell')} />
+        <button onClick={() => dispatch(increment())}>increment +</button> value: {value}   <button onClick={() => dispatch(decrement())}>decrement -</button>
       <Routes>
         <Route path="/" element={<NxWelcome title="shell" />} />
         <Route path="/orders" element={<Orders />} />
