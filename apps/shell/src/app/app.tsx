ea31b30f1eb2678afa57a5cb9cd071c9164b8c25
Arunesh Saxena <aruneshsaxena@gmail.com>
2026-02-02 10:33:44 +0530

    add cart mf app

--- apps/shell/src/app/app.tsx
+++ apps/shell/src/app/app.tsx
@@ -4,18 +4,23 @@ import { Link, Route, Routes } from 'react-router-dom';
 import { Button } from '@nx-repo/shared-ui';
 import { useDispatch, useSelector } from 'react-redux';
 import { increment, decrement } from './counter/counter.slice';
+
+const Cart = React.lazy(() => import('cart/Module'));
 const Orders = React.lazy(() => import('orders/Module'));
 
 const Products = React.lazy(() => import('products/Module'));
 
 export function App() {
-const value = useSelector((state: any) => state.counter.value);
-const dispatch = useDispatch();
+  const value = useSelector((state: any) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <React.Suspense fallback={null}>
       <ul>
         <li>
           <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
         </li>
         <li>
           <Link to="/orders">Orders</Link>
@@ -24,10 +29,15 @@ const dispatch = useDispatch();
           <Link to="/products">Products</Link>
         </li>
       </ul>
-      <Button label="Click Me shell" onClick={() => console.log('Button clicked! Shell')} />
-        <button onClick={() => dispatch(increment())}>increment +</button> value: {value}   <button onClick={() => dispatch(decrement())}>decrement -</button>
+      <Button
+        label="Click Me shell"
+        onClick={() => console.log('Button clicked! Shell')}
+      />
+      <button onClick={() => dispatch(increment())}>increment +</button> value:{' '}
+      {value} <button onClick={() => dispatch(decrement())}>decrement -</button>
       <Routes>
         <Route path="/" element={<NxWelcome title="shell" />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/products" element={<Products />} />
       </Routes>
